c5918bd11e9bca9135bed805b6c67b6b07967e87
Tanja <tignatova001@gmail.com>
2025-12-02 17:32:23 +0100

    socket creation

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -1,7 +1,10 @@
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
 
-
+# include <sys/socket.h>
+# include <iostream>
+# include <iomanip>
+# include <cmath>
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,13 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		return (1);
 
+		
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0)
+	{
+		std::cerr << "Socket creation failed.\n";
+		return (1);
+	}
 	return (0);
+
 }
